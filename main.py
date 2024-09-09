3cdf0d32b8300fd48e724f86d084bec00c374000
TatRa26 <tatra910@mail.ru>
2024-09-09 16:48:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Поймай снежинку")
