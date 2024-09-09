1b60a0f42491e4cd7b1d5ac118bf998e8dcf9291
TatRa26 <tatra910@mail.ru>
2024-09-09 16:41:28 +0300

    add new name game

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-pygame.display.set_caption("Игра Тир")
+pygame.display.set_caption("Игра Поймай снежинку")
 icon = pygame.image.load("img/content_7.jpg")
 pygame.display.set_icon(icon)
 
